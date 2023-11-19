9951b83eac9b688966ed0cb5fd38e699c244262a
daniellreyes <daniell.reyes26@gmail.com>
2023-11-19 14:28:40 -0600

    add docker files

--- web-server/main.py
+++ web-server/main.py
@@ -1,4 +1,21 @@
 import store
+from fastapi import FastAPI
+from fastapi.response import HTMLResponse
+
+app = FastAPI()
+
+@app.route('/')
+def get_list():
+    return [1, 2, 3]
+
+@app.route('/contact', response_class=HTMLResponse)
+def get_list():
+    return """
+        <h1>Hola mundoooo</h1>
+        <p>soy Daniel</p>
+    """
+
+
 
 def run():
     store.get_categories()
